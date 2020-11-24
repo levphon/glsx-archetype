7e6226c24c957679b53631c7466f801631a2a667
levphon <liuyufeng@outlook.com>
2020-11-25 01:05:20 +0800

    mybatis 公共方法

--- glsx-rest-archetype-framework/rest-admin-mybatis/src/main/java/com/glsx/plat/mybatis/mapper/CommonBaseMapper.java
+++ glsx-rest-archetype-framework/rest-admin-mybatis/src/main/java/com/glsx/plat/mybatis/mapper/CommonBaseMapper.java
@@ -7,5 +7,21 @@ public interface CommonBaseMapper<T> extends Mapper<T>, MySqlMapper<T> {
 
     //可以添加公共方法
 
+    default int deleteWithVersion(T t) {
+        int result = delete(t);
+        if (result == 0) {
+            throw new RuntimeException("删除失败!");
+        }
+        return result;
+    }
+
+    default int updateByPrimaryKeyWithVersion(T t) {
+        int result = updateByPrimaryKey(t);
+        if (result == 0) {
+            throw new RuntimeException("更新失败!");
+        }
+        return result;
+    }
+
 }
 
