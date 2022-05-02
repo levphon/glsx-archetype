fac2159f756dcfaa279bec27faef920174c157eb
levphon <liuyufeng@outlook.com>
2022-05-02 17:04:23 +0800

    框架完善-防重复提交

--- glsx-rest-archetype-framework/rest-admin-context/src/main/java/com/glsx/plat/context/aop/NoResubmitAspect.java
+++ glsx-rest-archetype-framework/rest-admin-context/src/main/java/com/glsx/plat/context/aop/NoResubmitAspect.java
@@ -3,7 +3,10 @@ package com.glsx.plat.context.aop;
 import com.glsx.plat.common.annotation.NoResubmit;
 import com.glsx.plat.core.web.R;
 import com.glsx.plat.web.utils.IpUtils;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.codec.digest.DigestUtils;
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
@@ -28,6 +31,10 @@ import java.util.concurrent.TimeUnit;
 @Component
 public class NoResubmitAspect {
 
+    final static GsonBuilder builder = new GsonBuilder();
+
+    final static Gson gson = builder.create();
+
     @Resource
     private StringRedisTemplate stringRedisTemplate;
 
@@ -46,47 +53,53 @@ public class NoResubmitAspect {
      * @return: java.lang.Object
      */
     @Around("@annotation(noResubmit)") //execution(* com.glsx.*.controller.*Controller.*(..))
-    public Object doAround(ProceedingJoinPoint pjp, NoResubmit noResubmit) {
-        try {
-            HttpServletRequest request = ((ServletRequestAttributes) Objects.requireNonNull(RequestContextHolder.getRequestAttributes())).getRequest();
+    public Object doAround(ProceedingJoinPoint pjp, NoResubmit noResubmit) throws Throwable {
 
-            // 拿到ip地址、请求路径、token
-            String ip = IpUtils.getIpAddr(request);
-            String url = request.getRequestURL().toString();
-            String token = request.getHeader(HttpHeaders.AUTHORIZATION);
+        HttpServletRequest request = ((ServletRequestAttributes) Objects.requireNonNull(RequestContextHolder.getRequestAttributes())).getRequest();
 
-            // 现在时间
-            long now = System.currentTimeMillis();
+        // 拿到ip地址、请求路径、token
+        String ip = IpUtils.getIpAddr(request);
+        String uri = request.getRequestURI();
+        String token = request.getHeader(HttpHeaders.AUTHORIZATION);
 
-            // 设置重复提交限制为3秒内
-            final int timeout = 3;
+        //参数签名
+        Object[] args = pjp.getArgs();
+        String argsJson = gson.toJson(args);
+        String sign = DigestUtils.md5Hex(argsJson);
 
-            // TODO 自定义key值方式,带上操作用户信息token
+        // 现在时间
+        long now = System.currentTimeMillis();
 
+        // 设置重复提交限制为3秒内
+        final int timeout = 3;
 
-            String key = "REQUEST_FORM_" + ip;
-            if (stringRedisTemplate.hasKey(key)) {
-                // 上次表单提交时间
-                long lastTime = stringRedisTemplate.getExpire(key, TimeUnit.MILLISECONDS);
-                // 如果现在距离上次提交时间小于设置的默认时间 则 判断为重复提交 否则 正常提交 -> 进入业务处理
-                if ((now - lastTime) > noResubmit.time()) {
-                    // 非重复提交操作 - 重新记录操作时间
-                    stringRedisTemplate.opsForValue().set(key, String.valueOf(now), timeout);
-                    // 进入处理业务
-                    R result = (R) pjp.proceed();
-                    return result;
-                } else {
-                    return R.error("请勿重复提交!");
-                }
-            } else {
-                // 这里是第一次操作
-                stringRedisTemplate.opsForValue().set(key, String.valueOf(now), timeout);
+        // TODO 自定义key值方式,带上操作用户信息token
+
+        String key = ip + "_" + uri + sign;
+
+        if (stringRedisTemplate.hasKey(key)) {
+            // 上次表单提交时间
+            // 上次表单提交时间
+            String lastTimeStr = stringRedisTemplate.opsForValue().get(key);
+            if (lastTimeStr == null) {
+                lastTimeStr = "0";
+            }
+            long lastTime = Long.parseLong(lastTimeStr);
+            // 如果现在距离上次提交时间小于设置的默认时间 则 判断为重复提交 否则 正常提交 -> 进入业务处理
+            if ((now - lastTime) > noResubmit.time()) {
+                // 非重复提交操作 - 重新记录操作时间
+                stringRedisTemplate.opsForValue().set(key, String.valueOf(now), timeout, TimeUnit.MILLISECONDS);
+                // 进入处理业务
                 R result = (R) pjp.proceed();
                 return result;
+            } else {
+                return R.error("请勿重复提交!");
             }
-        } catch (Throwable e) {
-            log.error("校验表单重复提交时异常: {}", e.getMessage());
-            return R.error("校验表单重复提交时异常!");
+        } else {
+            // 这里是第一次操作
+            stringRedisTemplate.opsForValue().set(key, String.valueOf(now), timeout, TimeUnit.MILLISECONDS);
+            R result = (R) pjp.proceed();
+            return result;
         }
     }
 
