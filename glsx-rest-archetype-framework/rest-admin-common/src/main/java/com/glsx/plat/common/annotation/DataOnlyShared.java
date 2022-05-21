184131075dcb0041abaf81a910db6c84ff0d4397
levphon <liuyufeng@outlook.com>
2022-05-21 21:25:41 +0800

    框架完善

---
+++ glsx-rest-archetype-framework/rest-admin-common/src/main/java/com/glsx/plat/common/annotation/DataOnlyShared.java
@@ -0,0 +1,44 @@
+package com.glsx.plat.common.annotation;
+
+import java.lang.annotation.*;
+
+/**
+ * 数据权限注解
+ *
+ * @author payu
+ */
+@Target({ElementType.METHOD})
+@Retention(RetentionPolicy.RUNTIME)
+@Inherited
+@Documented
+public @interface DataOnlyShared {
+
+    /**
+     * 需要进行过滤的连表后缀，完整表面为 业务表名_suffix
+     *
+     * @return
+     */
+    String permSuffix() default "_permit";
+
+    /**
+     * 连接数据权限的表，不设置默认会取主表
+     *
+     * @return
+     */
+    String linkTable() default "";
+
+    /**
+     * 需要进行过滤的连表id(增删改操作不需要)
+     *
+     * @return
+     */
+    String linkField() default "id";
+
+    /**
+     * 是否显示增强 sql 内容
+     *
+     * @return
+     */
+    boolean showSql() default false;
+
+}
---
+++ glsx-rest-archetype-framework/rest-admin-common/src/main/java/com/glsx/plat/common/model/DropOptions.java
@@ -0,0 +1,12 @@
+package com.glsx.plat.common.model;
+
+import lombok.Data;
+
+@Data
+public class DropOptions {
+
+    private Long id;
+    private String name;
+    private String code;
+
+}
